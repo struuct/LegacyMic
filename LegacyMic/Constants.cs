367b6c6547b9e838a56f853737fbb0c92d87ad72
struuct <134949919+struuct@users.noreply.github.com>
2026-05-14 16:27:51 -0500

    bump version

--- LegacyMic/Constants.cs
+++ LegacyMic/Constants.cs
@@ -2,7 +2,7 @@ namespace LegacyMic
 {
     internal abstract class Constants
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string ModName = "LegacyMic";
         public const string Guid = "struct.legacymic";
     }
